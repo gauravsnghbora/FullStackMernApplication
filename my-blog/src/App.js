52fba6e6ae4561103ff229ea6a97974b5f3ccd1a
Gaurav Bora <gauravsnghbora@gmail.com>
2024-09-04 07:30:37 +0530

    Front-end-setup

---
+++ my-blog/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlesListPage from "./pages/ArticlesListPage";
+import ArticlePage from "./pages/ArticlePage";
+import NavBar from "./NavBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFoundPage from "./pages/NotFoundPage";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/articles" element={<ArticlesListPage />} />
+            <Route path="/articles/:articleId" element={<ArticlePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
